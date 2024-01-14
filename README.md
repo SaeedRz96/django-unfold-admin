3539b2f6ec64edcb0df9cae531e386d58c35e216
saeedrz96 <saeedramezani75@gmail.com>
2024-01-14 17:13:08 +0330

    refactor: project file structure refactored

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# django-unfold-admin
-Unfold is theme for Django admin incorporating most common practises for building full-fledged admin areas. It is designed to work at the top of default administration provided by Django.
-
-<b>FORKED FROM https://github.com/unfoldadmin/django-unfold WITH RTL SUPPORT</b>
-<br>
-<hr>
-. How to use:
-<br>
-1. go steps from https://github.com/unfoldadmin/django-unfold#readme
-<br>
-2. just replace this repository files with pre installed files in step1. you can find it here: <i>/your_env_sitepackages/unfold</i>
+This is the django unfold admin package
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+
+setup(
+    name='django-unfold-admin',
+    version='0.1.0',
+    url='https://github.com/SaeedRz96/django-unfold-admin',
+    author='Saeed Ramazani',
+    author_email='saeedramezani75@gmail.com',
+    description='This is the django unfold admin package',
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+    packages=find_packages(),  
+    include_package_data=True,  
+    install_requires=[],
+)
